45ba82433a9dfc74fc90b04700e2d895bb21336e
wdm <850171861@qq.com>
2021-10-22 00:05:34 +0800

    模拟请求商品

--- src/components/productsList.vue
+++ src/components/productsList.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="products-card">
-  <a-card hoverable style="width: 240px">
+  <a-card hoverable style="width: 240px" v-for="item in productsData" :key="item.id">
     <template #cover>
-      <img alt="example" src="../assets/e7caa50e14ea52b3.jpeg" />
+      <img alt="example" :src="item.url" />
     </template>
-    <a-card-meta title="Apple 苹果 iPhone 13">
+    <a-card-meta :title="item.title">
       <template #description>
-        <p>¥7999.9</p>
+        <p>¥{{item.price}}</p>
         <a-button type="primary">加入购物车</a-button>
       </template>
     </a-card-meta>
   </a-card>
-
-    <a-card hoverable style="width: 240px">
-      <template #cover>
-        <img alt="example" src="../assets/mate.jpeg" />
-      </template>
-      <a-card-meta title="华为 Mate40 5G 手机">
-        <template #description>
-          <p>¥7999.9</p>
-          <a-button type="primary">加入购物车</a-button>
-        </template>
-      </a-card-meta>
-    </a-card>
-
-    <a-card hoverable style="width: 240px">
-      <template #cover>
-        <img alt="example" src="../assets/find.jpeg" />
-      </template>
-      <a-card-meta title="OPPO Find X3 Pro">
-        <template #description>
-          <p>¥7999.9</p>
-          <a-button type="primary">加入购物车</a-button>
-        </template>
-      </a-card-meta>
-    </a-card>
   </div>
 </template>
 
 <script lang="ts">
 
-import { defineComponent } from "vue";
+interface productsProps {
+  id: number;
+  price:number;
+  title:string;
+  url:string;
+}
+interface AxiosResponseProps{
+  code:number;
+  data:productsProps[]
+}
 
+import { defineComponent,onMounted,ref } from "vue";
 
+import axios from 'axios';
 export default defineComponent({
-  name: "productsList"
+  name: "productsList",
+  setup(){
+    // 获取商品数据
+    let productsData = ref<productsProps[]>([])
+    const getProductsData = async () =>{
+      const {data} = await axios.get<AxiosResponseProps>('/product.json')
+      if(data.code !== 0){
+        return alert("请求错误")
+      }
+      productsData.value = data.data
+    }
+
+
+    onMounted(() => {
+      getProductsData()
+    })
+
+    return {
+      productsData
+    }
+  }
 })
 </script>
 
