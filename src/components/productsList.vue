bbb9a41eef2d4d4f7cea63d5d4116b995d9c1371
wdm <850171861@qq.com>
2021-10-21 20:56:34 +0800

    init

---
+++ src/components/productsList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="products-card">
+  <a-card hoverable style="width: 240px">
+    <template #cover>
+      <img alt="example" src="../assets/e7caa50e14ea52b3.jpeg" />
+    </template>
+    <a-card-meta title="Apple 苹果 iPhone 13">
+      <template #description>
+        <p>¥7999.9</p>
+        <a-button type="primary">加入购物车</a-button>
+      </template>
+    </a-card-meta>
+  </a-card>
+
+    <a-card hoverable style="width: 240px">
+      <template #cover>
+        <img alt="example" src="../assets/mate.jpeg" />
+      </template>
+      <a-card-meta title="华为 Mate40 5G 手机">
+        <template #description>
+          <p>¥7999.9</p>
+          <a-button type="primary">加入购物车</a-button>
+        </template>
+      </a-card-meta>
+    </a-card>
+
+    <a-card hoverable style="width: 240px">
+      <template #cover>
+        <img alt="example" src="../assets/find.jpeg" />
+      </template>
+      <a-card-meta title="OPPO Find X3 Pro">
+        <template #description>
+          <p>¥7999.9</p>
+          <a-button type="primary">加入购物车</a-button>
+        </template>
+      </a-card-meta>
+    </a-card>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  name: "productsList"
+})
+</script>
+
+<style lang="scss" scoped>
+.products-card{
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="home-container">
+  <div class="products-header">
+    <span class="products-text">商品列表</span>
+    <span class="cart-button">
+      <a-badge class="tips-count" :count="5"></a-badge>
+      <a-button type="primary">我的购物车</a-button></span>
   </div>
+  </div>
+  <productsList />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
+
+import { defineComponent } from "vue";
+import productsList from "@/components/productsList.vue"
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    productsList
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.home-container{
+  .products-header{
+    padding-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+    .products-text{
+      font-size: 20px;
+      color: #333;
+    }
+    .cart-button{
+      position: relative;
+      .tips-count{
+        position: absolute;
+        right: -10px;
+        top: -12px;
+      }
+    }
+
+  }
+}
+
+</style>
